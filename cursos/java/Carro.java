fcfba5b8d1f49eaae377dbefdb62b2324d70376d
josedembo <josedembo18@aluno.unilab.edu.br>
2021-10-04 13:49:56 -0300

    adiciona nova pasta e arquivo

---
+++ cursos/java/Carro.java
@@ -0,0 +1,26 @@
+package cursos.java;
+
+import java.util.Scanner;
+
+public class Carro {
+
+    public static void name() {
+        Scanner leitor = new Scanner(System.in);
+        int valor1, valor2;
+
+        System.out.println("Infome um valor: ");
+        valor1 = leitor.nextInt();
+
+        System.out.println("Infome o valor2");
+        valor2 = leitor.nextInt();
+
+        System.out.println(soma(valor1, valor2));
+
+        leitor.close();
+    }
+
+    public static int soma(int valor1, int valor2) {
+        int soma = valor1 + valor2;
+        return soma;
+    }
+}
